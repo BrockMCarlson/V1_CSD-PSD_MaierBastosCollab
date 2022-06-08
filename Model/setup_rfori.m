7667466d06f90de279ed0b6a8668a5f76b2df4a3
MaxLichtenfeld <107149072+MaxLichtenfeld@users.noreply.github.com>
2022-06-08 15:22:23 -0500

    Update setup_rfori.m

--- Model/setup_rfori.m
+++ Model/setup_rfori.m
@@ -16,6 +16,12 @@ global RIGDIR CODEDIR OUTDIR_FD OUTDIR_PLOT
             OUTDIR_FD   = 'C:\Users\Brock\Documents\MATLAB\formattedDataOutputs\rfori from laminar labeling collaboration\';
             OUTDIR_PLOT = 'C:\Users\Brock\Documents\MATLAB\plotOutputs\plot outputs for laminar labeling collaboration\';
 
+            case {'MaxWork'}
+            RIGDIR      = 'E:\rig021_LaminarLabelingCollaboration\EndOfDayFileOutputs\  ';
+            CODEDIR     = 'C:\Users\Brock\Documents\MATLAB\GitHub\V1_CSD-PSD_MaierBastosCollab\Controller\';
+            OUTDIR_FD   = 'C:\Users\Brock\Documents\MATLAB\formattedDataOutputs\rfori from laminar labeling collaboration\';
+            OUTDIR_PLOT = 'C:\Users\Brock\Documents\MATLAB\plotOutputs\plot outputs for laminar labeling collaboration\';
+
     end
       
 cd(CODEDIR)
@@ -26,3 +32,8 @@ end
 % cd(CODEDIR)
 % ○
 % cd(OUTDIR_PLOT)
+
+
+
+
+

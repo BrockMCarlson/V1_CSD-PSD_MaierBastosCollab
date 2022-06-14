d3c8951aaa8cbbe4172c154fec18b0d4a445c273
MaxLichtenfeld <107149072+MaxLichtenfeld@users.noreply.github.com>
2022-06-14 12:28:31 -0500

    Max's first official push!

--- Controller/A_processNEV_rfori_copy.m
+++ Controller/A_processNEV_rfori_copy.m
@@ -1,3 +1,4 @@
+
 % processNEV_rfori()
 % plot PSD and CSD with NEV only - no stimulus information required
 
@@ -10,16 +11,14 @@
 %   2. All rfori files
 
 %%
-clc
+clear
 close all
-[RIGDIR, CODEDIR, OUTDIR_FD, OUTDIR_PLOT] = setup_rfori('BrockWork');
-cd(RIGDIR)
-
-user = input('User Number:  ');
+setup_rfori('Max');
 
 %% Automation setup 
 
 %get all LLC session names
+global RIGDIR
     fileList = dir(RIGDIR);
     fileList = {fileList.name};
     sessionListName = fileList(1,3:end)'; %this removes the first two enteries with dots (why does it do that?)
@@ -41,7 +40,7 @@ user = input('User Number:  ');
             sessionListName{sessionNumber},...
             '_rfori4lesions001');
     end
-    useChans  = {1:32,1:32,1:32,1:32,1:32,1:32,1:32,1:32,1:32,1:32,1:32,1:32,1:32,1:32,1:32}';
+    useChans  = {1:21,1:21,1:24,1:24,1:29,1:24,1:32,1:26,1:24,1:22,1:32,1:24,1:32,1:32,1:32}';
     qualityLaminarPenetration  = logical([0,0,0,0,1,0,1,1,1,1,1,1,1,1,1])';
     trueIfRforiWasRunToday = logical([0,0,1,0,1,1,1,0,1,1,0,1,1,1,0])';
     useDataInAnalysis = qualityLaminarPenetration & trueIfRforiWasRunToday;
@@ -49,11 +48,14 @@ user = input('User Number:  ');
     FileInformationTable = table(sessionListName,useDataInAnalysis, preLesionEvpFileName, rforiFileName, useChans,  interpTheseChans, qualityLaminarPenetration,trueIfRforiWasRunToday);
 
 
+% test test
 
 
+global OUTDIR_PLOT
 cd(OUTDIR_PLOT)
 for sessionNumber = 1:size(sessionListName,1)
     if sessionNumber == 7
+        warning('problem with session double')
         continue
 
     elseif FileInformationTable.useDataInAnalysis(sessionNumber)
@@ -62,7 +64,7 @@ for sessionNumber = 1:size(sessionListName,1)
 
         holderNameEVP = FileInformationTable.preLesionEvpFileName{sessionNumber};
         holderNameRfori = FileInformationTable.rforiFileName{sessionNumber};
-        holderUseChans = 1:24;   %FileInformationTable.useChans{sessionNumber};
+        holderUseChans = FileInformationTable.useChans{sessionNumber};
         holderInterpChans = FileInformationTable.interpTheseChans{sessionNumber};
          
         plotCSDandPSDfromNEV(holderNameEVP,holderUseChans,holderInterpChans);
@@ -72,7 +74,7 @@ for sessionNumber = 1:size(sessionListName,1)
               %saveas(gcf, strcat(OUTDIR_PLOT, FigName, '.png'));
               close all
               
-       plotCSDandPSDfromNEV(holderNameRfori,holderUseChans,holderInterpChans);
+        plotCSDandPSDfromNEV(holderNameRfori,holderUseChans,holderInterpChans);
               FigName   = strcat(sessionListName{sessionNumber},'_rfori');
               savefig(gcf, strcat(OUTDIR_PLOT, FigName, '.fig'));
               %saveas(gcf, strcat(OUTDIR_PLOT, FigName, '.svg'));
@@ -81,8 +83,8 @@ for sessionNumber = 1:size(sessionListName,1)
     end          
    
 end
-
 clc
+
 
 % 7 has a problem with rfori
 % 8 = 220216_B does not HAVE an rfori -- (did I get something wrong in
